a97ae06544cc91c90292ca7cf9a4fd24ba44f903
FF <80132594+fortesfr@users.noreply.github.com>
2022-03-12 12:57:28 -0500

    Update TM_Streamlit.py

--- TM_Streamlit.py
+++ TM_Streamlit.py
@@ -1,6 +1,6 @@
 #Streamlit App
 import streamlit as st
-# from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi
 # from youtube_transcript_api.formatters import JSONFormatter
 # import json
 # import transcriber
@@ -10,7 +10,7 @@ st.write("TestHiMeow")
 
 # texter = st.file_uploader("Upload Me", type ='txt')
 
-text = youtube_transcripter("GVsUOuSjvcg")
+# text = youtube_transcripter("GVsUOuSjvcg")
 # text = texter.read()
 
-st.write(text)
+# st.write(text)
