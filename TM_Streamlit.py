5b33984d28bae4d11a8f07ef3e4e0304d9f6219b
FF <80132594+fortesfr@users.noreply.github.com>
2022-03-12 17:21:36 -0500

    Update TM_Streamlit.py

--- TM_Streamlit.py
+++ TM_Streamlit.py
@@ -8,7 +8,7 @@ from transcriber import youtube_transcripter
 
 st.write("TestHiMeow")
 
-moo = st.text_input(label, value="YouTube Url", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False)
+moo = st.text_input(label, value="YouTube Url")
 st.write(moo)
 # texter = st.file_uploader("Upload Me", type ='txt')
 video_id = "oGb2oXZzIwY"
