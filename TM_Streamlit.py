ca3fb86e928d5275fed26ed853708920c436b6ac
FF <80132594+fortesfr@users.noreply.github.com>
2022-03-12 13:07:00 -0500

    Update TM_Streamlit.py

--- TM_Streamlit.py
+++ TM_Streamlit.py
@@ -1,6 +1,6 @@
 #Streamlit App
 import streamlit as st
-from youtube_transcript_api import YouTubeTranscriptApi
+# from youtube_transcript_api import YouTubeTranscriptApi
 # from youtube_transcript_api.formatters import JSONFormatter
 # import json
 import transcriber
