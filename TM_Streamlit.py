23f546c05255ce431afd376ac5b52b672c09c3a8
FF <80132594+fortesfr@users.noreply.github.com>
2022-03-12 12:31:48 -0500

    added libraries

--- TM_Streamlit.py
+++ TM_Streamlit.py
@@ -2,6 +2,9 @@
 
 import streamlit as st
 from transcriber import youtube_transcripter
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import JSONFormatter
+import json
 
 
 st.write("TestHiMeow")
