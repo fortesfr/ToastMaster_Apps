43c4d15b06df19cc0f92c1869f031e258e76ae37
FF <80132594+fortesfr@users.noreply.github.com>
2022-03-12 12:31:10 -0500

    py function and into streamlit

--- TM_Streamlit.py
+++ TM_Streamlit.py
@@ -1,12 +1,14 @@
 #Streamlit App
 
 import streamlit as st
+from transcriber import youtube_transcripter
 
 
 st.write("TestHiMeow")
 
-texter = st.file_uploader("Upload Me", type ='txt')
+# texter = st.file_uploader("Upload Me", type ='txt')
 
+texter = youtube_transcripter("GVsUOuSjvcg")
 text = texter.read()
 
 st.write(text)
--- transcriber.py
+++ transcriber.py
@@ -1,1 +1,18 @@
 
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import JSONFormatter
+import json
+
+video_id = "GVsUOuSjvcg"
+
+def youtube_transcripter(video_id):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    formatter = JSONFormatter()
+    json_formatted = formatter.format_transcript(transcript)
+
+    with open('your_filename.json', 'w', encoding='utf-8') as json_file:
+        json_file.write(json_formatted)
+
+    file = json_formatted.replace('\\n', ' ')
+    text_raw = json.loads(file)
+    return text_raw
